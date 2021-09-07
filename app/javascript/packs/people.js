d6b07d8111f027454329c94a39b04cd236fb5243
ASWIN KUMAR P M <aswin@techversantinfo.com>
2021-09-07 20:40:51 +0530

    updates

--- app/javascript/packs/people.js
+++ app/javascript/packs/people.js
@@ -1,40 +1,48 @@
-document.addEventListener("turbolinks:load", function() {
- $input = $("#tag-autocomplete")
+document.addEventListener("turbolinks:load", function () {
+    $input = $("#tag-autocomplete")
 
- console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+    console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
 
- var options = {
-  getValue: "full_name",
-  url: function(phrase) {
-    console.log(phrase)
-  return "/search.json?search=" + phrase;
- },
-categories: [
-  {
-    listLocation: "cameras",
-    header: "<strong>Cameras</strong>",
-  }
-],
-list: {
-  onChooseEvent: function() {
-    var url = $input.getSelectedItemData().url
-    console.log(url)
-    $input.val("")
-    Turbolinks.visit(url)
-  }
- }
-}
+    var options = {
+        getValue: "full_name",
+        url: function (phrase) {
+            console.log(phrase)
+            return "/search.json?search=" + phrase;
+        },
+        categories: [
+            {
+                listLocation: "cameras",
+                header: "<strong>Cameras</strong>",
+            }
+        ],
+        list: {
+            onChooseEvent: function () {
+                var url = $input.getSelectedItemData().url
+                console.log(url)
+                $input.val("")
+                Turbolinks.visit(url)
+            }
+        }
+    }
 
- var options_new = {
-    url: function(phrase) {
-      return "/search.json?search=" + phrase;
-    },
-    getValue: "name",
-  };
+    var options_new = {
+        url: function (phrase) {
+            return "/search.json?search=" + phrase;
+        },
+        getValue: "name",
+
+        // template: {
+        //     type: "links",
+        //     fields: {
+        //         link: "link"
+        //     }
+        // },
+    };
 
 
-  var options_test = {
-    data: ["John", "Paul", "George", "Ringo"]
-  };
+    var options_test = {
+        data: ["John", "Paul", "George", "Ringo"]
+    };
 
-$input.easyAutocomplete(options_new)});
+    $input.easyAutocomplete(options_new)
+});
--- app/views/friends/search.json.jbuilder
+++ app/views/friends/search.json.jbuilder
@@ -1,3 +1,5 @@
 json.array!(@friend) do |friend|
   json.name friend.first_name
+  json.link 'localhost:3000/search?search=' +
+                CGI.escape(friend.first_name)
 end
